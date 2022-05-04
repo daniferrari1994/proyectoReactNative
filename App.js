5a8a8c7a2bd358deaae15c603189f9ee186ffcb3
Dan Ferrari <daniferrari1994gmail.com>
2022-05-03 21:06:26 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Mi primer proyecto de React Native. Atte: Dan Ferrari</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#303030',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text:{
+    color: '#dc3545',
+  }
 });
